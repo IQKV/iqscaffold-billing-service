ee3b68b7788151440f72538f70348d58ccd72bd7
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2026-03-03 17:20:20 +0200

    chore(tenancy): change tenant resolution log level to INFO in `SchemaTenantIdentifierResolver`

--- src/main/java/com/iqscaffold/billingservice/tenancy/SchemaTenantIdentifierResolver.java
+++ src/main/java/com/iqscaffold/billingservice/tenancy/SchemaTenantIdentifierResolver.java
@@ -57,7 +57,7 @@ public class SchemaTenantIdentifierResolver implements CurrentTenantIdentifierRe
     var tenantId = TenantContext.getCurrentTenantId();
     var schemaName = schemaNameResolver.toSchema(tenantId);
     
-    logger.trace("Hibernate resolving tenant: {} → schema: {}", tenantId, schemaName);
+    logger.info("Hibernate resolving tenant: {} → schema: {}", tenantId, schemaName);
     
     return schemaName;
   }
